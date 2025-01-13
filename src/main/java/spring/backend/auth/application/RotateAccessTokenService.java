b7ddd0c361c74e9e3a67f572bcba67c1f18c3029
HoyeongJeon <hoyeong0302@gmail.com>
2025-01-13 13:25:11 +0900

    refactor: (#110) 토큰 재발급 로직을 변경한다.

--- src/main/java/spring/backend/auth/application/RotateAccessTokenService.java
+++ src/main/java/spring/backend/auth/application/RotateAccessTokenService.java
@@ -3,8 +3,9 @@ package spring.backend.auth.application;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
-import spring.backend.auth.presentation.dto.response.RotateAccessTokenResponse;
 import spring.backend.auth.exception.AuthenticationErrorCode;
+import spring.backend.auth.infrastructure.redis.repository.RefreshTokenRedisRepository;
+import spring.backend.auth.presentation.dto.response.RotateAccessTokenResponse;
 import spring.backend.core.application.JwtService;
 import spring.backend.member.domain.entity.Member;
 import spring.backend.member.domain.repository.MemberRepository;
@@ -20,29 +21,15 @@ public class RotateAccessTokenService {
     private final MemberRepository memberRepository;
     private final JwtService jwtService;
     private final RefreshTokenService refreshTokenService;
+    private final RefreshTokenRedisRepository refreshTokenRedisRepository;
 
-    public RotateAccessTokenResponse rotateAccessToken(String invalidAccessToken) {
-        UUID memberIdFromInvalidAccessToken = jwtService.extractMemberIdFromExpiredAccessToken(invalidAccessToken);
-        String refreshToken = refreshTokenService.getRefreshToken(memberIdFromInvalidAccessToken);
-        UUID memberId = extractMemberIdFromRefreshToken(refreshToken);
-        validateRefreshToken(memberId, refreshToken);
-        Member member = memberRepository.findById(memberId);
-        return new RotateAccessTokenResponse(jwtService.provideAccessToken(Optional.ofNullable(member).orElseThrow(MemberErrorCode.NOT_EXIST_MEMBER::toException)));
-    }
-
-    private UUID extractMemberIdFromRefreshToken(String refreshToken) {
-        if (refreshToken == null) {
-            log.error("쿠키에 refreshToken이 존재하지 않습니다.");
+    public RotateAccessTokenResponse rotateAccessToken(String refreshToken) {
+        if(refreshToken == null) {
             throw AuthenticationErrorCode.MISSING_COOKIE_VALUE.toException();
         }
-        return UUID.fromString(jwtService.getPayload(refreshToken).get("memberId", String.class));
-    }
-
-    private void validateRefreshToken(UUID memberId, String refreshToken) {
-        String savedRefreshToken = refreshTokenService.getRefreshToken(memberId);
-        if (!savedRefreshToken.equals(refreshToken)) {
-            log.error("리프레시 토큰이 저장소에 존재하지 않습니다.");
-            throw AuthenticationErrorCode.NOT_EXIST_REFRESH_TOKEN.toException();
-        }
+        refreshTokenService.validateRefreshToken(refreshToken);
+        UUID memberId = UUID.fromString(refreshTokenRedisRepository.findByRefreshToken(refreshToken));
+        Member member = memberRepository.findById(memberId);
+        return new RotateAccessTokenResponse(jwtService.provideAccessToken(Optional.ofNullable(member).orElseThrow(MemberErrorCode.NOT_EXIST_MEMBER::toException)));
     }
 }
