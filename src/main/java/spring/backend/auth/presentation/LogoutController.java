3bedc0ef514940b12ac82e83ec2f47f30fac5ceb
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-12 16:59:45 +0900

    refactor: 로그인 리턴값에 대한 새로운 Response Record를 생성한다.

--- src/main/java/spring/backend/auth/presentation/LogoutController.java
+++ src/main/java/spring/backend/auth/presentation/LogoutController.java
@@ -9,16 +9,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.backend.auth.application.RefreshTokenService;
 import spring.backend.auth.presentation.swagger.LogoutSwagger;
-import spring.backend.core.application.JwtService;
+import spring.backend.core.presentation.RestResponse;
 
 @RestController
 @RequiredArgsConstructor
 public class LogoutController implements LogoutSwagger {
     private final RefreshTokenService refreshTokenService;
-    private final JwtService jwtService;
 
     @PostMapping("/v1/logout")
-    public ResponseEntity<?> logout(
+    public ResponseEntity<RestResponse<Void>> logout(
             @CookieValue(name = "access_token", required = false) String accessToken,
             @CookieValue(name = "refresh_token", required = false) String refreshToken
     ) {
