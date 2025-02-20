5dfb5791eb74d42065c92bd7dec48e1de5d8ef26
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-20 15:55:28 +0900

    feat: 토큰 재발급 시 RTR을 적용한다.

--- src/main/java/spring/backend/auth/presentation/RotateAccessTokenController.java
+++ src/main/java/spring/backend/auth/presentation/RotateAccessTokenController.java
@@ -1,5 +1,6 @@
 package spring.backend.auth.presentation;
 
+import com.maxmind.geoip2.exception.GeoIp2Exception;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.backend.auth.application.RotateAccessTokenService;
-import spring.backend.auth.presentation.dto.response.RotateAccessTokenResponse;
-import spring.backend.auth.presentation.swagger.RotateAccessTokenSwagger;
+import spring.backend.auth.presentation.dto.response.RotateTokenResponse;
+import spring.backend.auth.presentation.swagger.RotateTokenSwagger;
+import spring.backend.core.configuration.argumentresolver.ClientIp;
 import spring.backend.core.presentation.RestResponse;
+
+import java.io.IOException;
 
 import static org.springframework.http.ResponseCookie.from;
 
@@ -20,21 +24,28 @@ import static org.springframework.http.ResponseCookie.from;
 @RequestMapping("/v1/token/rotate")
 @RequiredArgsConstructor
 @Log4j2
-public class RotateAccessTokenController implements RotateAccessTokenSwagger {
+public class RotateAccessTokenController implements RotateTokenSwagger {
     private final RotateAccessTokenService rotateTokenService;
 
     @PostMapping
-    public ResponseEntity<RestResponse<RotateAccessTokenResponse>> rotateAccessToken(
-            @CookieValue(name = "refresh_token", required = false) String refreshToken
-    ) {
-        RotateAccessTokenResponse rotateAccessTokenResponse = rotateTokenService.rotateAccessToken(refreshToken);
-        ResponseCookie newAccessToken = from("access_token", rotateAccessTokenResponse.accessToken())
+    public ResponseEntity<RestResponse<RotateTokenResponse>> rotateToken(
+            @CookieValue(name = "refresh_token", required = false) String refreshToken,
+            @ClientIp String ip
+    ) throws IOException, GeoIp2Exception {
+        RotateTokenResponse rotateTokenResponse = rotateTokenService.rotateToken(refreshToken, ip);
+        ResponseCookie newAccessToken = from("access_token", rotateTokenResponse.accessToken())
+                .httpOnly(true)
+                .path("/")
+                .build();
+
+        ResponseCookie newRefreshToken = from("refresh_token", rotateTokenResponse.refreshToken())
                 .httpOnly(true)
                 .path("/")
                 .build();
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, newAccessToken.toString())
+                .header(HttpHeaders.SET_COOKIE, newRefreshToken.toString())
                 .build();
     }
 }
