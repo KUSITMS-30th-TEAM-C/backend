53cb10b6d605a37eea7f4c87baadd32efb2cf432
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-20 15:56:12 +0900

    feat: 토큰 재발급 시 100 km 이상에서 들어온 요청에 대해 재발급을 반려한다.

--- src/main/java/spring/backend/auth/application/RotateAccessTokenService.java
+++ src/main/java/spring/backend/auth/application/RotateAccessTokenService.java
@@ -1,17 +1,18 @@
 package spring.backend.auth.application;
 
+import com.maxmind.geoip2.exception.GeoIp2Exception;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import spring.backend.auth.exception.AuthenticationErrorCode;
 import spring.backend.auth.infrastructure.redis.repository.RefreshTokenRedisRepository;
-import spring.backend.auth.presentation.dto.response.RotateAccessTokenResponse;
+import spring.backend.auth.presentation.dto.response.RotateTokenResponse;
+import spring.backend.core.application.GeoLocationService;
 import spring.backend.core.application.JwtService;
 import spring.backend.member.domain.entity.Member;
 import spring.backend.member.domain.repository.MemberRepository;
-import spring.backend.member.exception.MemberErrorCode;
 
-import java.util.Optional;
+import java.io.IOException;
 import java.util.UUID;
 
 @Service
@@ -22,14 +23,30 @@ public class RotateAccessTokenService {
     private final JwtService jwtService;
     private final RefreshTokenService refreshTokenService;
     private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+    private final GeoLocationService geoLocationService;
 
-    public RotateAccessTokenResponse rotateAccessToken(String refreshToken) {
-        if(refreshToken == null) {
+    public RotateTokenResponse rotateToken(String refreshToken, String newIp) throws IOException, GeoIp2Exception {
+        if (refreshToken == null) {
             throw AuthenticationErrorCode.MISSING_COOKIE_VALUE.toException();
         }
         refreshTokenService.validateRefreshToken(refreshToken);
+
+        String savedIp = jwtService.getPayload(refreshToken).get("ip", String.class);
+
+        if (geoLocationService.checkUserLocation(newIp, savedIp)) {
+            refreshTokenService.deleteRefreshToken(refreshToken);
+            log.error("100km 밖에서 토큰 재발급을 시도했습니다.");
+            throw AuthenticationErrorCode.TOKEN_ROTATE_ATTEMPT_FROM_100KM.toException();
+        }
+
         UUID memberId = UUID.fromString(refreshTokenRedisRepository.findByRefreshToken(refreshToken));
         Member member = memberRepository.findById(memberId);
-        return new RotateAccessTokenResponse(jwtService.provideAccessToken(Optional.ofNullable(member).orElseThrow(MemberErrorCode.NOT_EXIST_MEMBER::toException)));
+        String newAccessToken = jwtService.provideAccessToken(member);
+        String newRefreshToken = jwtService.provideRefreshToken(member, newIp);
+        refreshTokenService.saveRefreshToken(newRefreshToken, member);
+        return new RotateTokenResponse(
+                newAccessToken,
+                newRefreshToken
+        );
     }
 }
