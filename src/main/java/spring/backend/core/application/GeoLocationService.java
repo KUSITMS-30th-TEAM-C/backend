3465cc13063f243c460ea63814b02d302a4777a4
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-24 23:34:23 +0900

    feat: 토큰 재발급 유효 거리를 환경변수로 관리한다.

--- src/main/java/spring/backend/core/application/GeoLocationService.java
+++ src/main/java/spring/backend/core/application/GeoLocationService.java
@@ -4,8 +4,8 @@ import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CityResponse;
 import com.maxmind.geoip2.record.Location;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import spring.backend.core.util.geo.dto.response.Coordinate;
 
@@ -15,10 +15,15 @@ import java.net.InetAddress;
 import static spring.backend.core.util.geo.GeoUtil.calculateDistanceBetweenTwoCoordinate;
 
 @Service
-@RequiredArgsConstructor
 @Log4j2
 public class GeoLocationService {
     private final DatabaseReader reader;
+    private final int MAX_DISTANCE;
+
+    public GeoLocationService(@Value("${geo.max-distance}") int maxDistance, DatabaseReader reader) {
+        this.reader = reader;
+        this.MAX_DISTANCE = maxDistance;
+    }
 
     public boolean checkUserLocation(String newIp, String savedIp) throws IOException, GeoIp2Exception {
         Coordinate newIpCoordinate = getCoordinate(newIp);
@@ -31,7 +36,7 @@ public class GeoLocationService {
                 newIpCoordinate.longitude()
         );
 
-        return distanceBetweenIp > 100;
+        return distanceBetweenIp > MAX_DISTANCE;
     }
 
     private Coordinate getCoordinate(String ip) throws GeoIp2Exception, IOException {
